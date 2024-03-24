d87826b341fae482ca41df0a8dadbe2418c8f39c
anand <anandanand99599@gmail.com>
2024-03-24 10:44:05 +0530

    variable in terraform

--- variables/variables.tf
+++ variables/variables.tf
@@ -6,10 +6,22 @@ variable "y_list" {
   default = [10,20,30]
 }
 
+variable "z_map" {
+  default = {
+    a=5
+    b=3
+  }
+}
+
 output "x" {
   value = var.x
 }
 
-output "y_lisy" {
+output "y_list" {
   value = var.y_list
+}
+
+
+output "z_map" {
+  value = var.z_map["b"]
 }
