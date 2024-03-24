54364ea94e6a54bfcacf195a98cecdfbe3456877
anand <anandanand99599@gmail.com>
2024-03-24 10:23:13 +0530

    sample terraform

--- sample/sample.tf
+++ sample/sample.tf
@@ -1,8 +1,29 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-05f020f5935e52dc4"
   instance_type = "t3.micro"
+  vpc_security_group_ids = "sg-0a9bb227cbe599dab"
 
   tags = {
-    name = frontend
+    Name = "frontend"
+  }
+}
+
+resource "aws_instance" "backend" {
+  ami           = "ami-05f020f5935e52dc4"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = "sg-0a9bb227cbe599dab"
+
+  tags = {
+    Name = "backend"
+  }
+}
+
+resource "aws_instance" "mysql" {
+  ami           = "ami-05f020f5935e52dc4"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = "sg-0a9bb227cbe599dab"
+
+  tags = {
+    Name = "mysql"
   }
 }
